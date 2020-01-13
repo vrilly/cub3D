e4558013a06605ec248ec7ac40522c03cb00d5ad
Tom Jans <tjans@student.codam.nl>
2020-01-13 15:06:05 +0100

    Update libft + other ver of cub3d.c

--- src/cub3d.c
+++ src/cub3d.c
@@ -6,10 +6,11 @@
 /*   By: tjans <tjans@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/01/09 16:37:53 by tjans         #+#    #+#                 */
-/*   Updated: 2020/01/11 17:07:32 by tjans         ########   odam.nl         */
+/*   Updated: 2020/01/11 20:33:41 by tjans         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
 #include "cub3d.h"
 
 int main(void)
@@ -22,11 +23,7 @@ int main(void)
 	state.current_map = read_map_from_file("map.cub", &state);
 	if (!state.current_map)
 		return -1;
-	ft_putendl_fd("cub3D", 1);
-	ft_putnbr_fd(state.current_map->x_res, 1);
-	ft_putendl_fd(NULL, 1);
-	ft_putnbr_fd(state.current_map->y_res, 1);
-	ft_putendl_fd(NULL, 1);
+	printf("cub3D\ncolor test:\nF: %#x\nC: %#x\n", state.current_map->color_floor, state.current_map->color_ceiling);
 	free(state.current_map);
 	while (1) {}
 	return 0;
