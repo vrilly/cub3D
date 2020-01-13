f716a72e76b1518b0f9930c5a26f4ccdb27199c4
Tom Jans <tjans@student.codam.nl>
2020-01-13 16:03:47 +0100

    Set spawnpoint

--- inc/cub3d.h
+++ inc/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: tjans <tjans@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/01/11 16:55:04 by tjans         #+#    #+#                 */
-/*   Updated: 2020/01/11 21:24:02 by tjans         ########   odam.nl         */
+/*   Updated: 2020/01/13 16:00:36 by tjans         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,7 @@
 enum			e_map_tile_type {
 	TILE_EMPTY = 0,
 	TILE_WALL = 1,
-	TILE_ITEM = 2,
-	TILE_PLAYER = 3
+	TILE_ITEM = 2
 };
 
 typedef struct	s_map
@@ -39,6 +38,7 @@ typedef struct	s_map
 	unsigned int			map_width;
 	unsigned int			map_height;
 
+
 	unsigned int			color_floor;
 	unsigned int			color_ceiling;
 
@@ -51,6 +51,9 @@ typedef struct	s_game
 	t_map	*current_map;
 
 	void	*mlx_ptr;
+
+	float	camera_x;
+	float	camera_y;
 }				t_game;
 
 t_map			*read_map_from_file(char *path, t_game *state);
--- src/map/map_parser.c
+++ src/map/map_parser.c
@@ -6,7 +6,7 @@
 /*   By: tjans <tjans@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/01/13 13:37:55 by tjans         #+#    #+#                 */
-/*   Updated: 2020/01/13 15:01:41 by tjans         ########   odam.nl         */
+/*   Updated: 2020/01/13 16:03:38 by tjans         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,38 +19,55 @@ static int	calc_map_width(char *line)
 	map_width = 0;
 	while (*line)
 	{
-		if (*line >= '0' && *line <= '3')
+		if (*line >= '0' && *line <= '2')
 			map_width++;
 		line++;
 	}
 	return (map_width);
 }
 
-static int	parse_line(char *line, enum e_map_tile_type *mapdata)
+static int	is_pdir(char c)
+{
+	return (c == 'N' || c == 'S' || c == 'E' || c == 'W');
+}
+
+static void	set_spawn(char direction, unsigned int cl, int x, t_game *map)
+{
+	(void)direction;
+	map->camera_x = x;
+	map->camera_y = cl;
+}
+
+static int	parse_line(char *line, enum e_map_tile_type *mapdata,
+		unsigned int cl, t_game *state)
 {
 	int	line_c;
+	int	i;
 
-	while (*line)
+	i = 0;
+	while (line[i])
 	{
-		while (*line == ' ' && *(line + 1) != '\0')
-			line++;
-		line_c = *line - '0';
-		if (line_c < 0 || line_c > 3)
+		while (line[i] == ' ' && line[i + 1] != '\0')
+			i++;
+		line_c = line[i] - '0';
+		if (line_c < 0 || line_c > 2 || is_pdir(line_c))
 			return (0);
-		*mapdata = line_c;
+		if (is_pdir(line_c))
+			set_spawn(line_c, cl, i, state);
+		else
+			*mapdata = line_c;
 		mapdata++;
-		line++;
+		i++;
 	}
 	return (1);
 }
 
-int	map_reader_seq_mapdata(t_fdstream *fs, t_map *map, t_game *state)
+int			map_reader_seq_mapdata(t_fdstream *fs, t_map *map, t_game *state)
 {
 	unsigned int	curr_line;
 	char			*line;
 	int				ret;
 
-	(void)state;
 	curr_line = 0;
 	ret = fd_readline(fs, &line);
 	if (ret != 1)
@@ -61,7 +78,8 @@ int	map_reader_seq_mapdata(t_fdstream *fs, t_map *map, t_game *state)
 		map->mapdata = ft_realloc_safe(map->mapdata,
 			curr_line * map->map_width * sizeof(enum e_map_tile_type),
 			(curr_line + 1) * map->map_width * sizeof(enum e_map_tile_type));
-		if (!parse_line(line, map->mapdata + (curr_line * map->map_width)))
+		if (!parse_line(line, map->mapdata + (curr_line * map->map_width),
+					curr_line, state))
 			return (0);
 		ret = fd_readline(fs, &line);
 		curr_line++;
