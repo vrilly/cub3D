e3d9c104f1e3e90828314cebae24a6ae0c788b4a
Tom Jans <tjans@student.codam.nl>
2020-01-15 18:07:47 +0100

    Add frame funcs to hdr

--- inc/cub3d.h
+++ inc/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: tjans <tjans@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/01/11 16:55:04 by tjans         #+#    #+#                 */
-/*   Updated: 2020/01/15 17:28:13 by tjans         ########   odam.nl         */
+/*   Updated: 2020/01/15 18:07:42 by tjans         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -85,4 +85,6 @@ void			calc_step(t_game *state);
 void			calc_dda(t_game *state);
 void			prerendercalc(t_game *state);
 
+void			start_frame(t_game *state);
+void			end_frame(t_game *state);
 #endif
