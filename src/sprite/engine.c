1a4bf5b8abbf642fe3616387ac2a76d50ddd44b0
Tom Jans <tjans@student.codam.nl>
2020-02-03 22:32:57 +0100

    Remove wrong sprite addition
    I want to parse sprites in the mapparser for simplicity and performance
    reasons

--- src/sprite/engine.c
+++ src/sprite/engine.c
@@ -6,31 +6,32 @@
 /*   By: tjans <tjans@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/01/23 19:43:56 by tjans         #+#    #+#                 */
-/*   Updated: 2020/01/30 18:15:59 by tjans         ########   odam.nl         */
+/*   Updated: 2020/02/03 22:31:41 by tjans         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-void	init_sprite_engine(t_game *state)
-{
-	state->spr.zbuffer = ft_calloc(sizeof(double), state->current_map->x_res);
-	state->spr.sprites_size = 8;
-	state->spr.sprites = ft_calloc(sizeof(t_sprite), 8);
-}
-
-void	add_sprite(t_game *state, t_texture *tex, double x, double y)
+void	add_sprite(t_game *state, double x, double y)
 {
 	t_sprite_engine	*engine;
 
 	engine = &state->spr;
 	engine->sprites[engine->num_sprites]->x_pos = x;
 	engine->sprites[engine->num_sprites]->y_pos = y;
-	engine->sprites[engine->num_sprites]->texture = tex;
+	engine->sprites[engine->num_sprites]->texture =
+		&state->current_map->texture_sprite;
 	engine->num_sprites++;
 }
 
-void	render_sprites(t_game *state)
+void		init_sprite_engine(t_game *state)
+{
+	state->spr.zbuffer = ft_calloc(sizeof(double), state->current_map->x_res);
+	state->spr.sprites_size = 8;
+	state->spr.sprites = ft_calloc(sizeof(t_sprite), 8);
+}
+
+void		render_sprites(t_game *state)
 {
 	sort_sprites(&state->spr);
 }
