459e62b6281887016b00de474bb116a0d855a33e
Tom Jans <tjans@student.codam.nl>
2020-02-17 20:42:46 +0100

    Fix memleak, we don't need the bitmap anymore after saving

--- src/bmp/save_bmp.c
+++ src/bmp/save_bmp.c
@@ -6,7 +6,7 @@
 /*   By: tjans <tjans@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/02/17 03:56:22 by tjans         #+#    #+#                 */
-/*   Updated: 2020/02/17 20:23:22 by tjans         ########   odam.nl         */
+/*   Updated: 2020/02/17 20:42:32 by tjans         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,4 +22,6 @@ void	write_bitmap_to_file(char *path, t_bitmap *bmp)
 	write(fd, &bmp->info, 40);
 	write(fd, bmp->bitmap, (bmp->info.width * bmp->info.height * 4));
 	close(fd);
+	free(bmp->bitmap);
+	free(bmp);
 }
