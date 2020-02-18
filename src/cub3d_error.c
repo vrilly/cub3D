fa53d3560c9febee22d0b8f6e5f4cda057e4aa72
Tom Jans <tjans@student.codam.nl>
2020-02-18 19:02:59 +0100

    Better error msg

--- src/cub3d_error.c
+++ src/cub3d_error.c
@@ -6,7 +6,7 @@
 /*   By: tjans <tjans@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/02/04 06:15:19 by tjans         #+#    #+#                 */
-/*   Updated: 2020/02/18 16:27:56 by tjans         ########   odam.nl         */
+/*   Updated: 2020/02/18 19:01:28 by tjans         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,8 @@ char	g_errmsg[5][64] = {
 	"",
 	"MLX Failed to initialize",
 	"Couldn't read map",
-	"usage: ./cub3D \033[4;mmapfile\033[0;",
-	"Invalid arguments.\nusage: ./cub3D \033[4;mmapfile\033[0;"
+	"usage: ./cub3D [--save] \033[4;mmapfile\033[0m",
+	"Invalid arguments.\nusage: ./cub3D [--save] \033[4;mmapfile\033[0m"
 };
 
 void	*reterr(t_game *state, char *err)
