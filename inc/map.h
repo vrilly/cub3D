424fd3c6e27696c61dbddc60d6596270f2662034
Tom Jans <tjans@student.codam.nl>
2020-02-23 17:58:56 +0100

    Implemented a linked list for map reading
    mbuf_* functions for the linked list part.
    Left is implementation into the mapparser. Saved some logics into the line parsing which will be moved to the map vertification thingie :)

--- inc/map.h
+++ inc/map.h
@@ -6,21 +6,40 @@
 /*   By: tjans <tjans@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/01/09 19:15:01 by tjans         #+#    #+#                 */
-/*   Updated: 2020/02/17 01:37:03 by tjans         ########   odam.nl         */
+/*   Updated: 2020/02/23 17:55:51 by tjans         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MAP_SEQ_H
-# define MAP_SEQ_H
+#ifndef MAP_H
+# define MAP_H
 # include "cub3d.h"
 
-int	map_reader_seq_resolution(t_fdstream *fs, t_map *map, t_game *state);
-int	map_reader_seq_textures(t_fdstream *fs, t_map *map, t_game *state);
-int	map_reader_seq_colors(t_fdstream *fs, t_map *map, t_game *state);
-int	map_reader_seq_mapdata(t_fdstream *fs, t_map *map, t_game *state);
-int	map_flip(t_fdstream *fs, t_map *map, t_game *state);
+typedef struct			s_mapbuffer
+{
+	char				*line;
+	int					line_size;
+	int					line_size_max;
+	int					lines;
+	struct s_mapbuffer	*next;
+}						t_mapbuffer;
 
-int	load_texture(const char *ti, t_fdstream *fs, t_texture *dst, void *mlx);
-int	get_color_val(const char ti, t_fdstream *fs, unsigned int *dst);
+t_mapbuffer				*mbuf_create(char *line, int line_size);
+void					*mbuf_append(t_mapbuffer *root, t_mapbuffer *new);
+enum e_map_tile_type	*mbuf_finalize(t_mapbuffer *root);
+void					mbuf_destroy(t_mapbuffer *root);
+int						map_reader_seq_resolution(t_fdstream *fs,
+		t_map *map, t_game *state);
+int						map_reader_seq_textures(t_fdstream *fs,
+		t_map *map, t_game *state);
+int						map_reader_seq_colors(t_fdstream *fs,
+		t_map *map, t_game *state);
+int						map_reader_seq_mapdata(t_fdstream *fs,
+		t_map *map, t_game *state);
+int						map_flip(t_fdstream *fs, t_map *map, t_game *state);
+
+int						load_texture(const char *ti, t_fdstream *fs,
+		t_texture *dst, void *mlx);
+int						get_color_val(const char ti, t_fdstream *fs,
+		unsigned int *dst);
 
 #endif
---
+++ src/map/map_buffer.c
@@ -0,0 +1,91 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   map_buffer.c                                       :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: tjans <tjans@student.codam.nl>               +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2020/02/21 20:18:43 by tjans         #+#    #+#                 */
+/*   Updated: 2020/02/23 17:54:24 by tjans         ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "map.h"
+
+static void				copy_line(t_mapbuffer *node,
+		enum e_map_tile_type *mapdata)
+{
+	int		i;
+	char	c;
+
+	i = 0;
+	while (i < node->line_size)
+	{
+		c = node->line[i];
+		while (c == ' ' && node->line[i + 1] != '\0')
+			i++;
+		*mapdata = c -
+				((c == 'N' || c == 'S' || c == 'E' || c == 'W') ? 0 : '0');
+		mapdata++;
+		i++;
+	}
+}
+
+t_mapbuffer				*mbuf_create(char *line, int line_size)
+{
+	t_mapbuffer	*buff;
+
+	buff = ft_calloc(1, sizeof(t_mapbuffer));
+	if (!buff)
+		return (NULL);
+	buff->line = line;
+	buff->line_size = line_size;
+	buff->lines = 1;
+	return (buff);
+}
+
+void					*mbuf_append(t_mapbuffer *root, t_mapbuffer *new)
+{
+	root->lines++;
+	if (new->line_size > root->line_size_max)
+		root->line_size_max = new->line_size;
+	while (root->next)
+		root = root->next;
+	root->next = new;
+}
+
+enum e_map_tile_type	*mbuf_finalize(t_mapbuffer *root)
+{
+	int						line_size;
+	int						lines;
+	int						i;
+	enum e_map_tile_type	*mapdata;
+
+	line_size = root->line_size_max;
+	lines = root->lines;
+	mapdata = ft_calloc(lines, line_size * sizeof(enum e_map_tile_type));
+	i = 0;
+	while (i < lines)
+	{
+		copy_line(root, mapdata + (line_size * i));
+		i++;
+		root = root->next;
+	}
+	return (mapdata);
+}
+
+void					mbuf_destroy(t_mapbuffer *root)
+{
+	t_mapbuffer	*prevnode;
+
+	prevnode = root;
+	while (prevnode->next)
+	{
+		root = prevnode->next;
+		free(prevnode->line);
+		free(prevnode);
+		prevnode = root;
+	}
+	free(prevnode->line);
+	free(prevnode);
+}
