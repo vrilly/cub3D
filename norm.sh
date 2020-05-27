89f24ac519a44e4ac4a9db078fdedc23db4fffd2
Tom Jans <tjans@student.codam.nl>
2020-05-27 16:18:14 +0200

    Don't grep norminette output; better for evals

--- norm.sh
+++ norm.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-find inc src libft plugins -type f \( -iname "*.c" -or -iname "*.h" \) -exec norminette {} \; | grep -B1 -i error
+find inc src libft plugins -type f \( -iname "*.c" -or -iname "*.h" \) -exec norminette {} \;
