1f214f266a094a621fafcf12db70785e5ea9bdf1
Tom Jans <tjans@student.codam.nl>
2020-05-27 13:39:17 +0200

    Add stub for when program is compiled without bonus

---
+++ src/plugin.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   plugin_bonus.c                                     :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: tjans <tjans@student.codam.nl>               +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2020/05/05 12:00:00 by tjans         #+#    #+#                 */
+/*   Updated: 2020/05/05 12:00:00 by tjans         ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cub3d.h"
+
+enum e_cub_error	init_plugins(t_game *state)
+{
+	(void)state;
+	ftlog(LOG_INFO, "Plugin support disabled.");
+	return (NO_ERROR);
+}
