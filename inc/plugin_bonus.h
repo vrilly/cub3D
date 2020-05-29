72907a31d1830c9c6dd4cbe8a523adac81a81a4d
Tom Jans <tjans@student.codam.nl>
2020-05-29 12:32:08 +0200

    Finished documenting all headers.

--- inc/plugin_bonus.h
+++ inc/plugin_bonus.h
@@ -15,10 +15,22 @@
 
 typedef struct s_game	t_game;
 
+/*
+** Plugin metadata.
+** At this moment only contains the plugin name
+** In the future it will be able to hold a bit more stuff like configuration
+** and more metadata
+*/
+
 typedef struct			s_plugininfo
 {
 	char	*name;
 }						t_plugininfo;
+
+/*
+** Plugin struct
+** Contains all hooks plugins can provide
+*/
 
 typedef struct			s_plugin
 {
@@ -34,13 +46,29 @@ typedef struct			s_plugin
 	int				(*pregame_hook)(t_game *state, void *pl_state);
 }						t_plugin;
 
+/*
+** Linked list for all loaded plugins.
+*/
+
 typedef struct			s_pluginlist
 {
 	t_plugin			*plugin;
 	struct s_pluginlist	*next;
 }						t_pluginlist;
 
+/*
+** Loads config.txt in plugins/ and loads all dynamic libraries
+** Upon loading it initializes struct, resolves symbols for hooks and
+** adds it to the linked list in the game struct
+*/
+
 enum e_cub_error		init_plugins(t_game *state);
+
+/*
+** Functions which iterates through the loaded plugins and executes the hooks
+** Error handling here is very basic. Could've been done better tbh
+*/
+
 void					execute_frame_hook(t_game *state);
 void					execute_pregame_hook(t_game *state);
 
